ac385dad490915690c6cb7648e988019d3b53517
nathan <nathanjshaffer@gmail.com>
2024-10-05 12:50:57 -0400

    first working commit

--- src/mqttdataclient.cpp
+++ src/mqttdataclient.cpp
@@ -1,29 +1,64 @@
+#include "PubSubClient.h"
 #include "mqttdataclient.h"
 
 
-void MQTTDataClient::SetDeviceId(string id){deviceId = id;}
+void MQTTDataClient::SetDataTopic(String topic)
 {
-  subscribe(codeTopic + "/+/command", [] (const String& topic, const String& message)
+  this->dataTopic = topic;
+  // this->data = JSON.parse("{}");
+  
+  setMaxPacketSize(1028);
+  subscribe(this->dataTopic + "/dataclient/update", [this] (const String& topic, const String& message)
   {
-    String name = topic.substring(codeTopic.length() + 1, topic.length() - 8);
-    Serial.print(name);
-    
-    JSONVar cmd = JSON.parse(message)
-    // JsonDocument doc;
-    // 
-    // // Deserialize the JSON document
-    // DeserializationError error = deserializeJson(doc, message);
-    // 
-    // // Test if parsing succeeds.
-    // if (error) {
-    //   Serial.print(F("deserializeJson() failed: "));
-    //   Serial.println(error.f_str());
-    //   return;
-    // }
+    Serial.println(message.length());
+    JSONVar cmd = JSON.parse(message);
 
-    if (cmd["cmd"] == "update")
+    if (cmd.hasOwnProperty("name") && cmd.hasOwnProperty("data"))
     {
-      data[name] = cmd["data"];
+      String name = cmd["name"];
+      this->SaveData(name, cmd["data"]);
     }
   });
+
+
+  subscribe(this->dataTopic + "/dataclient/get", [this] (const String& topic, const String& message)
+  {
+    JSONVar cmd = JSON.parse(message);
+    if (cmd.hasOwnProperty("name"))
+    {
+      String name = cmd["name"];
+      JSONVar response;
+      response["name"] = name;
+      response["data"] = LoadData(name);
+      publish(this->dataTopic + "/dataclient/data",  JSON.stringify(response));
+    }
+  });
+}
+
+void MQTTDataClient::SetStorePermanent(Preferences* prefs)
+{
+  storePermanent = true;
+  preferences = prefs;
+}
+
+void MQTTDataClient::SaveData(String name, JSONVar d)
+{
+  if (this->data.hasOwnProperty(name))
+  {
+    if(this->data[name] == d) return;
+  }
+  this->data[name] = d;
+  if(storePermanent && this->preferences != null)
+  {
+    this->preferences->putString(name.c_str(), JSON.stringify(d));
+  }
+}
+
+JSONVar MQTTDataClient::LoadData(String name)
+{
+  if (!this->data.hasOwnProperty(name) && storePermanent && this->preferences != null)
+  {
+    this->data[name] = JSON.parse(this->preferences->getString(name.c_str()));
+  }
+  return this->data[name]; 
 }
--- src/mqttdataclient.h
+++ src/mqttdataclient.h
@@ -1,16 +1,29 @@
 #include <Arduino.h>
 #include "EspMQTTClient.h"
 #include <Arduino_JSON.h>
+#include <Preferences.h>
 
+#ifndef MQTT_DATA_CLIENT
+#define MQTT_DATA_CLIENT
 
 
 class MQTTDataClient : public EspMQTTClient
 {
   using EspMQTTClient::EspMQTTClient;
+  String dataTopic;
+  bool storePermanent = false;
+  Preferences* preferences = null;
 
-  String deviceId;
-  JSONVar data;
+  public:
+    JSONVar data;
+    void SetDataTopic(String topic);
+    void SaveData(String name, JSONVar d);
+    void SetStorePermanent(Preferences* prefs);
+    JSONVar LoadData(String name);
+    JSONVar GetData(String name){return this->data[name];}
+    // void SavePermanent(String name, JSONVar d);
+};
 
-  void SetDeviceId(string id){deviceId = id;}
-}
+// JSON_Compare(JSONVar)
 
+#endif
